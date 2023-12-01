08e91e906245f55c8f149aea2f722bf96ea9128a
Cesar Zagonel <zagonelcesar@gmail.com>
2023-12-01 16:12:08 -0300

    redis tls

--- src/redis.ts
+++ src/redis.ts
@@ -6,6 +6,9 @@ export default async function getRedis() {
   if (!client) {
     client = createClient({
       url: process.env.REDIS_URL,
+      socket: {
+        tls: process.env.REDIS_TLS == "true",
+      },
     });
     client.on("error", (err) => console.log("Redis Client Error", err));
     await client.connect();
