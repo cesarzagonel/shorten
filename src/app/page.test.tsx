ee0607c9735f0efd9efd2b37c23bc42d7c7c759c
Cesar Zagonel <zagonelcesar@gmail.com>
2023-12-03 23:37:24 -0300

    more tests

---
+++ src/app/page.test.tsx
@@ -0,0 +1,48 @@
+import "@/test/dbTestCase";
+import "@testing-library/jest-dom";
+
+import { ReactNode } from "react";
+import { QueryClient, QueryClientProvider } from "react-query";
+import { screen } from "@testing-library/react";
+import Home from "./page";
+import prisma from "@/prisma";
+import UrlList from "@/components/UrlList";
+import { renderServerComponent } from "@/test/renderServerComponent";
+
+jest.mock("../helpers/currentUser");
+jest.mock("../helpers/sessionId");
+jest.mock("../components/UrlList");
+
+const client = new QueryClient();
+
+function Wrapper({ children }: { children: ReactNode }) {
+  return <QueryClientProvider client={client}>{children}</QueryClientProvider>;
+}
+
+it("should render", async () => {
+  await renderServerComponent(<Home />, Wrapper);
+
+  expect(screen.getByText("Free")).toBeInTheDocument();
+});
+
+it("should render session urls", async () => {
+  const session = "session-a";
+
+  const url = await prisma.url.create({
+    data: {
+      id: "url-a",
+      title: "url a",
+      url: "localhost",
+      session,
+    },
+    include: {
+      _count: {
+        select: { visits: true },
+      },
+    },
+  });
+
+  await renderServerComponent(<Home />, Wrapper);
+
+  expect(UrlList).toHaveBeenCalledWith({ urls: [url] }, {});
+});
